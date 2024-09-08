1f3af2c1fbecde2834790120f18745295b56bf3c
Atharva Wandhare <162739542+WandhareAtharva@users.noreply.github.com>
2024-09-08 17:26:11 +0530

    create a basic user model

---
+++ backend/src/models/user.model.js
@@ -0,0 +1,77 @@
+import mongoose, { Schema } from "mongoose";
+import jwt from 'jsonwebtoken';
+import bcrypt from 'bcrypt';
+
+const UserSchema = new Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+        trim: true,
+        index: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+        trim: true,
+    },
+    fullname: {
+        type: String,
+        required: true,
+        trim: true,
+        index: true,
+    },
+    avatar: {
+        type: String, // cloudinary
+        required: true,
+    },
+    password: {
+        type: String,
+        required: [true, "Password is required!!!"],
+    },
+    refreshToken: {
+        type: String,
+    }
+}, { timestamps: true });
+
+UserSchema.pre('save', async function (next) {
+    if (!this.isModified("password")) return next();
+    this.password = await bcrypt.hash(this.password, 10);
+    next();
+});
+
+UserSchema.methods.isPasswordCorrect = async function (password) {
+    return await bcrypt.compare(password, this.password)
+}
+
+UserSchema.methods.generateAccessToken = function (password) {
+    return jwt.sign(
+        {
+            _id: this._id,
+            email: this.email,
+            username: this.username,
+            fullname: this.fullname,
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+        }
+    )
+}
+
+UserSchema.methods.generateRefreshToken = function (password) {
+    return jwt.sign(
+        {
+            _id: this._id,
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+        }
+    )
+}
+
+export const User = mongoose.model("User", UserSchema);
