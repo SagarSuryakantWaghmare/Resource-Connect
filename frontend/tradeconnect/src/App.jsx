c33378d8ad25f79ae1794ab84077cc57f4c801c5
SwapnilSalunke1625 <salunkeswapnil145@gmail.com>
2024-09-13 23:18:59 +0530

    made loginpage responsive

--- frontend/tradeconnect/src/App.jsx
+++ frontend/tradeconnect/src/App.jsx
@@ -1,14 +1,14 @@
 import './App.css';
-import Navbar from './components/Navbar';
+// import Navbar from './components/Navbar';
 // import UserSignUp from './Pages/UserSignUp'
 // import SignLog from './Pages/SignLog'
-import HomePage from './Pages/HomePage';
+// import HomePage from './Pages/HomePage';
 import LoginPage from './Pages/LoginPage'
 
 function App() {
   return (
     <>
-      <Navbar />
+      {/* <Navbar /> */}
       {/* <HomePage /> */}
       <LoginPage/>
 
--- frontend/tradeconnect/src/Pages/LoginPage.jsx
+++ frontend/tradeconnect/src/Pages/LoginPage.jsx
@@ -3,35 +3,45 @@ import React from 'react'
 export default function LoginPage() {
     return (
         <>
-            <div className='flex items-center justify-center h-screen font-stdFont bg-stdBg'>
-                <div className="rounded-2xl w-[450px] h-2/3 text-center bg-white">
-                    <h1 className='text-headTag font-bold text-stdYellow'>Welcome</h1>
-                    <p className='text-paraFont mt-1 text-stdBlue font-semibold'>Wellcome to our community of skilled proffessionals!</p>
-                    <div className="flex flex-col gap-4  justify-center items-center mt-10 ">
-                        <input type="email" placeholder='Email' className='h-inpH w-inpW border rounded-2xl pl-3 outline-none' />
-                        <input type="password" placeholder='Password' className='h-inpH w-inpW border rounded-2xl pl-3 outline-none' />
-                    </div>
-                    <p className='text-right mr-5 mt-2 text-paraFont'><a href="#">Forgot Password?</a></p>
-                    <div className="mt-5">
-                        <button className='h-inpBtnH w-[150px] bg-stdBlue rounded-2xl  text-primaryFont font-bold text-stdYellow'>Login</button>
-                    </div>
+         {/* Outer div for screen */}
+            <div className='bg-stdBg flex items-center justify-center min-h-screen font-stdFont  p-4'>
 
-                    <p className='text-paraFont mt-2'>Don't have an account?<a href="#" className='text-secondaryFont font-semibold text-stdBlue'> Sign Up</a></p>
+         {/* Login page div  */}
+         
+                <div className="rounded-2xl w-full max-w-[450px] md:h-[65%] h-auto text-center bg-white p-6">
+                    <h1 className='text-3xl md:text-4xl font-bold text-stdYellow'>Welcome</h1>
+                    <p className='text-[12px] md:text-[14px] mt-1 text-stdBlue font-semibold'>
+                        Welcome to our community of skilled professionals!
+                    </p>
+                    <div className="flex flex-col gap-4 justify-center items-center mt-10">
+                        <input type="email" placeholder='Email' className='h-[45px] w-full max-w-[300px] md:max-w-[350px] border rounded-2xl pl-3 outline-none text-sm md:text-base' />
+                        <input type="password" placeholder='Password' className='h-[45px] w-full max-w-[300px] md:max-w-[350px] border rounded-2xl pl-3 outline-none text-sm md:text-base' />
+                    </div>
+                    <p className='text-right  mt-2 md:text-sm text-paraFont'>
+                        <a href="#">Forgot Password?</a>
+                    </p>
+                    <div className="mt-5">
+                        <button className='h-[45px] w-[120px] bg-stdBlue rounded-full text-[18px] font-bold text-stdYellow'>
+                            Login
+                        </button>
+                    </div>
+                    <p className='text-[12px] md:text-sm mt-2'>
+                        Don't have an account? <a href="#" className='text-[14px] font-semibold text-stdBlue'>Sign Up</a>
+                    </p>
                     <fieldset className='mt-2'>
                         <legend>OR</legend>
                     </fieldset>
-                    <div className="flex flex-col gap-2 justify-center items-center mt-3">
-                        <button className='w-inpBtnW h-inpBtnH border border-stdBlue rounded-lg font-semibold'>
+                    <div className="flex flex-col gap-2 justify-center items-center mt-2">
+                        <button className='w-full max-w-[250px] md:max-w-[280px] h-[40px] border border-stdBlue rounded-xl font-semibold'>
                             <i className="fa-brands fa-google mr-2"></i>
                             Login with Google
                         </button>
-                        <button className='w-inpBtnW h-inpBtnH border border-stdBlue rounded-lg  font-semibold'>
+                        <button className='w-full max-w-[250px] md:max-w-[280px] h-[40px] border border-stdBlue rounded-xl font-semibold'>
                             <i className="fa-brands fa-apple mr-2.5 text-ternaryFont"></i>
                             Login with Apple
                         </button>
                     </div>
                 </div>
-
             </div>
         </>
     )
