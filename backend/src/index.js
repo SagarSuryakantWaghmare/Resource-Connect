f7652bd21404c002e46d28af89e284e6660f829e
Atharva Wandhare <162739542+WandhareAtharva@users.noreply.github.com>
2024-10-16 17:07:37 +0530

    Refactor package.json and index.js files

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,9 +2,7 @@
 import dotenv from "dotenv"
 import connectDB from "./db/index.js";
 import { app } from './app.js'
-dotenv.config({
-    path: './.env'
-})
+dotenv.config()
 
 
 
@@ -21,27 +19,4 @@ connectDB()
     })
     .catch((err) => {
         console.log("MONGO db connection failed !!! ", err);
-    })
-
-/*
-import express from "express"
-const app = express()
-( async () => {
-    try {
-        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
-        app.on("errror", (error) => {
-            console.log("ERRR: ", error);
-            throw error
-        })
-
-        app.listen(process.env.PORT, () => {
-            console.log(`App is listening on port ${process.env.PORT}`);
-        })
-
-    } catch (error) {
-        console.error("ERROR: ", error)
-        throw err
-    }
-})()
-
-*/
+    });
