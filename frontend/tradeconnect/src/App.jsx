15503f4532fb0d4cd96a8951676e20b09d0cf37c
Atharva Wandhare <162739542+WandhareAtharva@users.noreply.github.com>
2024-10-17 23:38:40 +0530

    organize app.jsx for proper looks

--- frontend/tradeconnect/src/App.jsx
+++ frontend/tradeconnect/src/App.jsx
@@ -11,24 +11,27 @@ import Services from './Pages/Services';
 import LandingPageUser from './Pages/LandingPageUser';
 import UserProfile from './Pages/UserProfile';
 import ChatPage from './Pages/ChatPage';
-import BookPage from './Pages/BookPage';
 import MyBookings from './Pages/MyBookings';
+import WorkerSignUp from './Pages/WorkerSignUp';
 
 function App() {
   return (
     <Router>
       <Navbar /> 
       <Routes>
-       
+
+       {/* Before Login Links are the following*/}
         <Route path="/" element={<HomePage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/signup" element={<UserSignUp />} />
         <Route path="/signlog" element={<SignLog />} />
         <Route path="/services" element={<Services />} /> 
+        <Route path="/signup-w" element={<WorkerSignUp />} /> 
+
+        {/* After Login Routes are these ones */}
         <Route path="/home" element={<LandingPageUser />} />
         <Route path="/profile" element={<UserProfile />} />
         <Route path="/chat" element={<ChatPage />} />
-        <Route path="/book" element={<BookPage />} />
         <Route path="/my-bookings" element={<MyBookings />} />
 
       </Routes>
