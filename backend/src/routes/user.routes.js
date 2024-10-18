bdaf23ec7fdf9e53216773e220eb8e207dab30da
Atharva Wandhare <162739542+WandhareAtharva@users.noreply.github.com>
2024-10-19 04:19:17 +0530

    Refactor user.routes.js to add postJob and getJobPosts routes

--- backend/src/routes/user.routes.js
+++ backend/src/routes/user.routes.js
@@ -9,6 +9,8 @@ import {
     updateAccountDetails,
     updateUserAvatar,
     updateUserCoverImage,
+    postJob,
+    getJobPosts
 } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js"
 import { verifyJWT } from "../middlewares/auth.middleware.js";
@@ -34,5 +36,7 @@ router.route("/current-user").get(verifyJWT, getCurrentUser);
 router.route("/update-account").patch(verifyJWT, updateAccountDetails);
 router.route("/avatar").patch(verifyJWT, upload.single("avatar"), updateUserAvatar);
 router.route("/cover-image").patch(verifyJWT, upload.single("coverImage"), updateUserCoverImage);
+router.route("/job-post").post(verifyJWT, postJob);
+router.route("/get-job-posts").get(verifyJWT, getJobPosts);
 
 export default router;
