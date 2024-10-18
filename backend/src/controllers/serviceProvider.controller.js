4363fdbdcfa6f4e01146429b7ea30f3c5061a696
Atharva Wandhare <162739542+WandhareAtharva@users.noreply.github.com>
2024-10-18 20:28:24 +0530

    Add ServiceProvider model import to serviceProvider.controller.js

--- backend/src/controllers/serviceProvider.controller.js
+++ backend/src/controllers/serviceProvider.controller.js
@@ -2,6 +2,7 @@ import { asyncHandler } from "../utils/asyncHandler.js";
 import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import { User } from "../models/user.model.js";
+import { ServiceProvider } from "../models/serviceProvider.js";
 import { Review } from "../models/review.model.js";
 
 // Controller for fetching service providers by city
