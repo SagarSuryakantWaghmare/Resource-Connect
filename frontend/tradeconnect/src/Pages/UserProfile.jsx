33d6adf6e8c05f601502cce26e34d98e62547b97
Atharva Wandhare <162739542+WandhareAtharva@users.noreply.github.com>
2024-10-20 09:28:31 +0530

    Refactor UserProfile component: Update import paths for ProfileReview and ProfilePhoto components

--- frontend/tradeconnect/src/Pages/UserProfile.jsx
+++ frontend/tradeconnect/src/Pages/UserProfile.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import ProfileReview from "../components/ProfileReview";
-import ProfilePhoto from "../components/Assets/profileImage.jpg"
+import ProfileReview from "../components/ProfileReview.jsx";
+import ProfilePhoto from "../components/Assets/ProfileImage.jpg";
 
 
 export default function UserProfile(props) {
